465122eb225294c2dc017bff4d0b3e96182f1b73
key-on423 <3531697760@qq.com>
2024-12-15 19:55:01 +0800

    数据结构

---
+++ C算法与数据结构001/C算法与数据结构001/源代码.c
@@ -0,0 +1,19 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	printf("请输入你想求的累计数和（如1到100的数和）\n");
+//	scanf("%d", &i);
+//	//方法一
+//	/*int sum = 0;
+//	for (int w = 0; w <= i; w++)
+//	{
+//		sum += w;
+//	}*/
+//	//高级算法
+//	int sum = 0;
+//	sum = ((i + 1) * i) / 2;
+//
+//	printf("1到%d的累计和为:%d",i, sum);
+//}
